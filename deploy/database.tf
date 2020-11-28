80e697f95f6be1c15744f7918d044b58816f6869
YeseungPark <parkyes90@gmail.com>
2020-11-28 16:59:17 +0900

    Added ECS Service

--- deploy/database.tf
+++ deploy/database.tf
@@ -20,7 +20,9 @@ resource "aws_security_group" "rds" {
     protocol  = "tcp"
     to_port   = 5432
     security_groups = [
-    aws_security_group.bastion.id]
+      aws_security_group.bastion.id,
+      aws_security_group.ecs_service.id
+    ]
   }
   tags = local.common_tags
 }
