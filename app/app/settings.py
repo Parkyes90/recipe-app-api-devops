1fcee58219796c5a58236e4933fd22e7f582680d
YeseungPark <parkyes90@gmail.com>
2020-11-29 18:04:25 +0900

    Configured S3 image backend

--- app/app/settings.py
+++ app/app/settings.py
@@ -139,3 +139,14 @@ MEDIA_ROOT = '/vol/web/media'
 STATIC_ROOT = '/vol/web/static'
 
 AUTH_USER_MODEL = 'core.User'
+
+S3_STORAGE_BACKEND = bool(int(os.environ.get("S3_STORAGE_BACKEND", 1)))
+if S3_STORAGE_BACKEND is True:
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3BotoStorage"
+
+AWS_DEFAULT_ACL = "public-read"
+AWS_STORAGE_BUCKET_NAME = os.environ.get("S3_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.environ.get(
+    "S3_STORAGE_BUCKET_REGION", "ap-northeast-2"
+)
+AWS_QUERYSTRING_AUTH = False
