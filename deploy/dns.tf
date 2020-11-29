25600c901b6ac54f68d36537f210a94a817fc684
YeseungPark <parkyes90@gmail.com>
2020-11-29 18:51:03 +0900

    Added Dns config to terraform

---
+++ deploy/dns.tf
@@ -0,0 +1,34 @@
+data "aws_route53_zone" "zone" {
+  name = "${var.dns_zone_name}."
+}
+
+resource "aws_route53_record" "app" {
+  name    = "${lookup(var.subdomain, terraform.workspace)}.${data.aws_route53_zone.zone.name}"
+  type    = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl     = "300"
+  records = [aws_lb.api.dns_name]
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = aws_route53_record.app.fqdn
+  validation_method = "DNS"
+  tags              = local.common_tags
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.zone.zone_id
+  records = [
+  aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
+  ttl = "60"
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+}
